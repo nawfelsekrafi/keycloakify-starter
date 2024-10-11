5e4021b289d9d496bba2c527f77263b9395d8c60
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2024-10-11 11:27:06 +0100

    updates

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -4,6 +4,11 @@ import type { ThemeName } from "../kc.gen";
 /** @see: https://docs.keycloakify.dev/i18n */
 const { useI18n, ofTypeI18n } = i18nBuilder
     .withThemeName<ThemeName>()
+    .withCustomTranslations({
+        "ar": {
+            "login.backToLogin": "العودة إلى تسجيل الدخول"
+        }
+    })
     .build();
 
 type I18n = typeof ofTypeI18n;
